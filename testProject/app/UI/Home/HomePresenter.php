e8b0edf8806f25f1e578fd53e905600c6fecaa40
Petr Svoboda <peetr.svoboda@seznam.cz>
2024-09-10 15:27:01 +0200

    first commit

---
+++ testProject/app/UI/Home/HomePresenter.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\UI\Home;
+
+use App\Model\PostFacade;
+use Nette;
+
+final class HomePresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private PostFacade $facade,
+	) {
+	}
+
+	public function renderDefault(): void
+	{
+		$this->template->posts = $this->facade
+			->getPublicArticles()
+			->limit(5);
+	}
+}
---
+++ testProject/app/UI/Post/PostPresenter.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\UI\Post;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class PostPresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+	public function renderShow(int $postId): void
+{
+	$post = $this->database
+		->table('posts')
+		->get($postId);
+	if (!$post) {
+		$this->error('Stránka nebyla nalezena!!!');
+	}
+
+	$this->template->post = $post;
+		$this->template->comments = $post->related('comments')->order('created_at');
+}
+
+// vytvoření komponenty na komentáře
+
+protected function createComponentCommentForm() : Form {
+  $form = new Form;
+
+  $form->addText('name', 'Jméno:')
+  ->setRequired();
+
+$form->addEmail('email','E-mail:');
+
+$form->addTextArea('content', 'Komentář:')
+->setRequired();
+
+$form->addSubmit('send', 'Publikovat komentář');
+
+$form->onSuccess[] = $this->commentFormSucceeded(...);
+
+return $form;
+}
+
+//odeslání do db
+
+private function commentFormSucceeded(\stdClass $data): void
+{
+	$postId = $this->getParameter('postId');
+
+	$this->database->table('comments')->insert([
+		'post_id' => $postId,
+		'name' => $data->name,
+		'email' => $data->email,
+		'content' => $data->content,
+	]);
+
+	$this->flashMessage('Děkuji za komentář', 'success');
+	$this->redirect('this');
+}
+
+}
+
+
